9065d885d261d68f4cfb5bebe851d47768181a18
Chijith Jerin <chijithjerin2004@gmail.com>
2022-04-03 15:19:23 +0530

    Initial commit

---
+++ market/route.py
@@ -0,0 +1,101 @@
+from unicodedata import name
+from market import app
+from flask import render_template, redirect,url_for,flash,get_flashed_messages, request
+from market.model import Item,User
+from market.forms import LoginForm, RegisterForm, PurchaseItemForm, SellItemForm
+from market import db
+from flask_login import login_user,logout_user, login_required, current_user
+import os
+
+
+
+@app.route("/") # are called as decorator
+@app.route("/home")
+def home_page():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'CJ_Logo_2.png')
+    return render_template("home.html",comp_logo=pic1)
+
+@app.route("/market",methods=['GET','POST'])
+@login_required
+def market_page():
+    purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
+    if request.method == 'POST':
+        #purchase item login
+        purchase_item = request.form.get('purchase_item')
+        p_item_object = Item.query.filter_by(name=purchase_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"Congratulations! you purchased {p_item_object.name} for {p_item_object.price}$", category='success')
+
+            else:
+                flash(f"Unfortunately, you dont't enough money to purchase {p_item_object.name} for {p_item_object.price}$", category='danger')
+        
+        #sold item login
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You Sold {s_item_object.name} back to market!", category='success')
+
+            else:
+                flash(f"Something went wrong while selling {s_item_object.name}", category='danger')
+
+        return redirect(url_for("market_page"))
+    
+    if request.method == "GET":    
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        items = Item.query.filter_by(owner=None)
+        return render_template("market.html",items=items,purchase_form=purchase_form, owned_items = owned_items, selling_form=selling_form) 
+
+@app.route("/register" ,methods=['GET','POST'])
+def register_page():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'CJ_Logo_2.png')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, 
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data,)
+                              #password=form.password1.data) :}  uncomment this if you are a baby
+        
+        db.session.add(user_to_create)
+        db.session.commit()
+        
+        login_user(user_to_create)
+        flash(f"Account Created Successfully! You are now logged in as {user_to_create.username}", category='success')
+
+        return redirect(url_for('market_page'))
+
+    if form.errors != {}: # If there is errors from the validations
+        for err_msg in form.errors.values():
+            flash(f"There was an unexpected foolish error{err_msg}", category='danger')
+
+    return render_template("register.html", form=form, comp_logo=pic1)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login_page():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'CJ_Logo_2.png')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        attempted_password = User.query.filter_by(password_hash=form.password.data).first()
+        if attempted_password and attempted_user:
+            login_user(attempted_user)
+            flash(f"Succesfully logged in as {attempted_user.username}", category='success')
+            return redirect(url_for("market_page"))
+
+        else:
+            flash("Username and password are not match! Please try again", category="danger")
+
+    return render_template("login.html", form=form, comp_logo=pic1)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("Logged out successfully!", category="info")
+    return redirect(url_for('home_page'))
