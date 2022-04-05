2fbf906415788d6ab0a345a1751176166f204f63
Chijith Jerin <chijithjerin2004@gmail.com>
2022-04-05 06:54:38 +0530

    ..

--- market/forms.py
+++ market/forms.py
@@ -1,4 +1,3 @@
-from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
