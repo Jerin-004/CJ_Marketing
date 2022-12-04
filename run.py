01c009276d4e86bd5b7e567ce016d742c5b00c54
Chijith Jerin <chijithjerin2004@gmail.com>
2022-12-05 03:48:36 +0530

    main update

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from market import app
-import sys
- 
 
-sys.path.insert(0, 'CJ_Marketing/market')
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=False,host="0.0.0.0")
